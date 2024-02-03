33dc1d59c8de294d5c170a1c54ae53e1e016f6b9
avanimathur <avanimathur24@gmail.com>
2024-02-03 22:52:08 +0530

    MetroStations Choice Dropdown

---
+++ script.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const dataUrl = "METRO_DATA.xlsx";
+
+    function fetchData(url, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+        xhr.responseType = "arraybuffer";
+
+        xhr.onload = function (e) {
+            var arraybuffer = xhr.response;
+            var data = new Uint8Array(arraybuffer);
+            var arr = [];
+            for (var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+            var bstr = arr.join("");
+            var workbook = XLSX.read(bstr, { type: "binary" });
+            var firstSheetName = workbook.SheetNames[0];
+            var worksheet = workbook.Sheets[firstSheetName];
+            var jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+            callback(jsonData);
+        };
+
+        xhr.send();
+    }
+
+    function populateDropdown(elementId, data) {
+        const dropdown = document.getElementById(elementId);
+        data.forEach((row) => {
+            const option = document.createElement("option");
+            option.value = row[0]; 
+            option.text = row[0];
+            dropdown.appendChild(option);
+        });
+    }
+
+    fetchData(dataUrl, function (data) {
+        populateDropdown("from", data);
+        populateDropdown("to", data);
+    });
+});
